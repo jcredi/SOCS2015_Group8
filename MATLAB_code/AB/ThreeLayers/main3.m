92079bc6e22896aa7d8a0c059ebfd7e93746477b
Viktor <wanerlov@gmail.com>
2015-12-10 18:15:00 +0100

    Probably done:)

--- MATLAB_code/AB/ThreeLayers/main3.m
+++ MATLAB_code/AB/ThreeLayers/main3.m
@@ -9,6 +9,8 @@ nWarehouses = 10; % number of stores
 nManufacturers = 5;
 worldSize = 1; %size of the world
 maxDistance = 3; %interaction radius (UNUSED)
+alpha = 5;
+probabilityGain = 0.01;
 
 retailersDemands = ones(1,nRetailers);
 warehousesMaxCapacity = Inf;%(nRetailers/nWarehouses)*ones(nWarehouses,1);
