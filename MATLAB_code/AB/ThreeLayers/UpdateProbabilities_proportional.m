4226dd1d073630151661e7607fccfbd211d86ab9
Jacopo Credi <jacopo.credi@gmail.com>
2015-12-11 16:06:11 +0100

    proportional update

---
+++ MATLAB_code/AB/ThreeLayers/UpdateProbabilities_proportional.m
@@ -0,0 +1,19 @@
+function updatedProbabilities = UpdateProbabilities_proportional(probabilities, shipments, ...
+    requestsMatrix, distances, alpha, scalingFactor)
+
+nCustomers = size(probabilities,2);
+updatedProbabilities = probabiities;
+
+
+for iCustomer = 1:nCustomers
+    selectedSupplier = find(requestsMatrix(:,iCustomer));
+    if shipments(iCustomer) ~= 0 % if this customer got some items
+        shipmentDistance = distances(selectedSupplier, iCustomer);
+        profit = shipments(iCustomer) - alpha*shipmentDistance;
+        
+        updatedProbabilities(selectedSupplier, iCustomer) = ...
+                updatedProbabilities(selectedSupplier, iCustomer)+scalingFactor*profit;
+    end
+end
+
+end
